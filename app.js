5394efd7f0148fc2e51666932a1aadcd2ad59532
Hackim18 <khakim668@gmail.com>
2024-08-24 20:12:35 +0700

    feat: add bin/www

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send({ message: "Hello World!" });
+  res.json({ message: "Hello world" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
